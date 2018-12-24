524b0c5907528317f5fab38ff08c145e4f747409
lhm <3186834196@qq.com>
2018-12-24 14:28:09 +0800

    完善测试demo

---
+++ app/src/main/java/com/lhm/view/easyphotosex/InstallProvider.java
@@ -0,0 +1,19 @@
+package com.lhm.view.easyphotosex;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Create on 2017/9/4 下午2:25
+ * <p>
+ * author wang
+ * <p>
+ * Description: 读取下载apk时使用的provider
+ * <p>
+ * Version: 1.0.4
+ */
+public class InstallProvider extends FileProvider {
+
+    /**
+     * 处理FileProvider 多个的问题
+     */
+}
--- app/src/main/java/com/lhm/view/easyphotosex/MainActivity.kt
+++ app/src/main/java/com/lhm/view/easyphotosex/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.lhm.view.easyphotosex
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import com.huantansheng.easyphotos.EasyPhotos
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +17,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
     }
+
+
+    fun camera(view :View){
+        EasyPhotos.createCamera(this)
+                .setFileProviderAuthority("com.lhm.view.easyphotosex.provider")
+                .start(1)
+
+    }
+
+    fun album(view :View){
+        EasyPhotos.createAlbum(this, false, GlideEngine.getInstance())
+                .setFileProviderAuthority("com.lhm.view.easyphotosex.provider")//参数说明：见下方`FileProvider的配置`
+                .setCount(9)//参数说明：最大可选数，默认1
+                .start(1)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                GlideEngine.getInstance().loadPhoto(this,
+                        data!!.getStringArrayListExtra(EasyPhotos.RESULT_PATHS)[0],iv)
+            }
+        }
+    }
+
+
 
 }
